60ebff4a7c3c07786cde1ca5a1bf80c0cf8346da
Michal Czyzewski <czy.mich@gmail.com>
2019-04-23 23:34:47 +0200

    add else

--- src/pl/mczyzewski/MinutesToYearsAndDaysCalculator.java
+++ src/pl/mczyzewski/MinutesToYearsAndDaysCalculator.java
@@ -5,23 +5,25 @@ public class MinutesToYearsAndDaysCalculator
 
     public static void printYearsAndDays(long minutes)
     {
-        if (minutes < 0)
+        if (minutes <= 0)
         {
             System.out.println("Invalid Value");
-        }
-
-        long year = minutes / 525600;
-        long days = minutes % 525600 / 1440;
-
-        if (year >= 1)
-        {
-
-            System.out.println(minutes + " min = " + year + " y and " + days + " d");
         } else
         {
 
-            days = minutes / 1440;
-            System.out.println(minutes + " min = " + year + " y and " + days + " d");
+            long year = minutes / 525600;
+            long days = minutes % 525600 / 1440;
+
+            if (year >= 1)
+            {
+
+                System.out.println(minutes + " min = " + year + " y and " + days + " d");
+            } else
+            {
+
+                days = minutes / 1440;
+                System.out.println(minutes + " min = " + year + " y and " + days + " d");
+            }
         }
     }
 
